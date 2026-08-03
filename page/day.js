598dc7603b4855ab7b6da3c14a6494b503a3c685
Milan Vukasinovic <milanvukasinovic@tuta.com>
2026-08-03 17:30:24 +0200

    img load successfull

--- page/day.js
+++ page/day.js
@@ -9,13 +9,13 @@ Page({
     const month = now.getMonth() + 1 // Monate sind 0-indexiert
     const year = now.getFullYear()
     const dayOfWeek = now.getDay() // 0 (Sonntag) bis 6 (Samstag)
-    
+
     // Prüfen, ob heute Mittwoch (3) oder Freitag (5) ist
     const isWednesdayOrFriday = dayOfWeek === 3 || dayOfWeek === 5
-    
+
     // Datum im Format TT/MM/JJJJ formatieren
     const dateText = `${day.toString().padStart(2, '0')}/${month.toString().padStart(2, '0')}/${year}`
-    
+
     // Datum anzeigen
     const dayText = createWidget(widget.TEXT, {
       x: 96,
@@ -30,33 +30,30 @@ Page({
       text: dateText
     })
 
+
+
     // Konvertierte Bilder anzeigen
     if (isWednesdayOrFriday) {
       // Fastentag: Fisch und Wein/Brot anzeigen
-      if (global.fishConvertedPath) {
-        hmUI.createWidget(hmUI.widget.IMG, {
-          x: 100,
-          y: 150,
-          src: global.fishConvertedPath
+      const fried_chicken = createWidget(widget.IMG, {
+        x: 125,
+        y: 125,
+        src: 'fried-chicken.png'
+      })
+
+        const fish = createWidget(widget.IMG, {
+          x: 125,
+          y: 125,
+          src: 'fish.png'
         })
-      }
-      
-      if (global.wineAndBreadConvertedPath) {
-        hmUI.createWidget(hmUI.widget.IMG, {
-          x: 200,
-          y: 150,
-          src: global.wineAndBreadConvertedPath
-        })
-      }
+
     } else {
       // Kein Fastentag: Frittiertes Hähnchen anzeigen
-      if (global.friedChickenConvertedPath) {
-        hmUI.createWidget(hmUI.widget.IMG, {
-          x: 150,
-          y: 150,
-          src: global.friedChickenConvertedPath
-        })
-      }
+      const wineandbread = createWidget(widget.IMG, {
+        x: 125,
+        y: 125,
+        src: 'wineandbread.png'
+      })
     }
   }
 })
